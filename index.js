1682b80f4e444f7b77973e2a1b0ef828eba83555
tlannigan <tristanlannigan@gmail.com>
2020-03-18 14:03:00 -0600

    Add initial listener and setup environment

---
+++ index.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    console.log(message.content);
+    
+});
+
+client.login(process.env.BOT_TOKEN);
